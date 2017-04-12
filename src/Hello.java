035fd44d60a0b27946505cc21f30fc536536f6cc
SHEFFcode <sheff@sheffmachine.com>
2017-04-11 21:58:20 -0700

    First mess around with Java

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        //width of 32
+        int myValue = 1000; // pretty big
+        //width of 8
+        byte myByte = 10; //about 128 to - 128
+        byte newByte = (byte)(myByte / 2); //specific type casting in java
+        //width of 16
+        short myShortVaule = 32; //twice the space of a byte
+        //long has a width of 64
+        long myLongValue = 100L;
+    }
+}
