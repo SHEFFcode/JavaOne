271b3b1a1e73e3186b9704842af0d4d7e9db3563
SHEFFcode <sheff@sheffmachine.com>
2017-04-19 21:59:22 -0700

    boxing and unboxing

--- src/Arrays/BoxingUnboxing.java
+++ src/Arrays/BoxingUnboxing.java
@@ -10,6 +10,10 @@ public class BoxingUnboxing {
         String[] strArray = new String[10];
         ArrayList<Integer> intArrayLst = new ArrayList<>();
         //ponint is auto boxing and unboxing is available only to primative wrappers like Integer for int
+
+
+        intArrayLst.add(5);
+        intArrayLst.get(0);
     }
 
 
