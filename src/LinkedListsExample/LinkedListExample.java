43d64e3a4f3f0a3c39c01ed847a20abf08cd9069
SHEFFcode <sheff@sheffmachine.com>
2017-04-20 21:36:29 -0700

    Use list iterator for linked lists

--- src/LinkedListsExample/LinkedListExample.java
+++ src/LinkedListsExample/LinkedListExample.java
@@ -2,6 +2,7 @@ package LinkedListsExample;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Created by jeremy.shefer on 4/20/17.
@@ -31,5 +32,28 @@ public class LinkedListExample {
             System.out.println("Now visiting " + i.next());
         }
         System.out.println("=============================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);//0 is match
+            if (comparison == 0) {
+                //equal do not add
+                System.out.println(newCity + " is already a destination.");
+                return false;
+            } else if (comparison > 0) {
+                //new city should appear before this one
+                //Brisbane > Adalae, would return a number greater then 0, because B is greater then A
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                //Just move on to the next city, this is not the place to add the new city.
+            }
+        }
+        //we went through the whole list, so we need to add it to the end
+        stringListIterator.add(newCity);
+        return true;
     }
 }
